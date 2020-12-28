1013f80f432a9c9ed7ee9a067803d54da3aa3409
Chun-Hui Gao <gaospecial@gmail.com>
2020-12-28 22:40:37 +0800

    add tag_by_regex() & shrink_lvl()

--- content.R
+++ content.R
@@ -22,4 +22,44 @@ extract_from_hist_graph <- function(M=NULL, g=NULL){
   M %>% filter(DI %in% doi)
 }
 
+# 根据检索词对文献进行分类
+#' Tag record by regular expression search
+#'
+#' @param x 
+#' @param name the name of new column
+#' @param pattern 
+#' @param pattern.names 
+#' @param sep default is ";"
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tag_by_regex <- function(x, pattern, pattern.names = names(pattern), sep = ";"){
+  require(stringr)
+  nRecord <- length(x)
+  result <- vector("list", length = nRecord)
+  nPattern <- length(pattern)
+  for (i in 1:nRecord){
+    this_record <- x[[i]]
+    idx <- vector(length = nPattern)
+    for (j in 1:nPattern){
+      this_pattern <- pattern[[j]]
+      if (!is.na(this_record) & str_detect(this_record, this_pattern)) idx[[j]] <- TRUE
+    }
+    result[[i]] <- paste0(pattern.names[idx], collapse = sep)
+  }
+  return(unlist(result))
+}
 
+
+# 将排在后面的项目合并成 others
+shrink_lvl <- function(x, sort = TRUE, keep = 9, other = "Other"){
+  if (is.factor(x)) x <- as.character(x)
+  lvl <- names(sort(table(x), decreasing = TRUE))
+  lvl_new <- head(lvl, n = keep)
+  ret <- rep(other, times = length(x))
+  idx <- x %in% lvl_new
+  ret[idx] <- x[idx]
+  return(ret)
+}
